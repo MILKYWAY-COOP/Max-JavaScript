0e53074fbe3733657c86db00626a35ce35644775
MaxwellNdegwa <86935948+MaxwellNdegwa@users.noreply.github.com>
2022-05-11 14:40:38 +0300

    Delete JS Syntax directory

--- JS Syntax/Section 1-Basics/playground.js
+++
@@ -1,6 +0,0 @@
-console.log(100); 
-console.log(student);
-
-/* this sample was a test to see whether i must define my values and apparently
-  yes, i have to.
-this applies to strings and not integers. */ 
