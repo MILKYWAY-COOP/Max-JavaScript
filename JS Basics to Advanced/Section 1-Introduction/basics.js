6ed1b877a68fad4371bc7d4bead7b6761d04deb1
MaxwellNdegwa <86935948+MaxwellNdegwa@users.noreply.github.com>
2022-05-11 14:40:09 +0300

    Add files via upload

---
+++ JS Basics to Advanced/Section 1-Introduction/basics.js
@@ -0,0 +1,16 @@
+console.clear();
+/*clears the console evertime the code is run.*/
+
+
+var num1 = 23;
+var num2 = 43;
+var num3 = 66; //this can be termed as an expression as it contains; a variable, value and an operator
+
+var sum = num1 + num2 + num3;
+
+console.log(sum);
+
+/*var firstname = "'Maxwell'" // Values enclosed in the quotes is treated as a string.
+console.log(firstname);*/
+
+//strings,numbers,boolean etc are datatypes that tell JS what type of value have been stored in the variables.
